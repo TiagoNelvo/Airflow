6a79f82e8337c131a20fc32376d519a305e31c7b
TiagoNelvo <ti.609@hotmail.com>
2024-05-04 02:04:55 -0300

    Hooks

---
+++ dags/hook_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+data = "/mnt/c/Users/Tilto/Desktop/Work/Airflow/data"
+
+def read_data():
+    pghook = PostgresHook(postgres_conn_id="PG_SWORDBLAST")
+    pghook.copy_expert(
+        "COPY (SELECT * FROM characters) TO stdout WITH CSV HEADER",
+        data + "/characters.csv",
+    )
+
+def copy_data():
+    lines = ""
+    with open(data + "/characters.csv", "r") as f:
+        for lines in f:
+            lines += line.replace(",", "-")
+    with open(data + "/characters.csv", "w") as f:
+        f.wirte(lines)
+
+
+with DAG(
+    dag_id="Hook_dag", schedule_interval=None, start_date=datetime(2020, 1, 1)
+) as dag:
+
+    task1 = PythonOperator(task_id="task1", python_callable=read_data)
+
+    task2 = PythonOperator(task_id="task2", python_callable=copy_data)
+
+    task1 >> task2
