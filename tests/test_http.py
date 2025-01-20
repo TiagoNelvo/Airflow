c976a8f4243accb4daab6d9e4b993894b4ff89de
TiagoNelvo <ti.609@hotmail.com>
2025-01-20 09:47:31 -0300

    Mocking

---
+++ tests/test_http.py
@@ -0,0 +1,35 @@
+import pytest 
+
+
+def test_http_operator(test_dag,mocker):
+    mocker.path.object(
+        BaseHook,
+        "get_connection",
+        return_value=Connection(scheama="https",host="api.sunrise-sunset.org")
+    )
+
+def _check_light(sunset_sunrise_response):
+    results = sunset_sunrise_response.json()["results"]
+    sunrise = datetime.strptime(results["sunrise"][:-6], "%Y-%m-%dT%H:%M:%S")
+    sunset = datetime.strptime(results["sunset"][:-6], "%Y-%m-%dT%H:%M:%S")
+    
+    if sunrise < datetime.utcnow() < sunset:
+        return False
+    else:
+        return True
+    
+is_light = SimpleHttpOpreator(
+    task_id="is_light",
+    http_conn_id="descomplica"
+    endpoint="json",
+    method="GET",
+    data={"lat": "52.370216","lng":"4,895168","formatted":"0"},
+    response_check=_check_light()
+    dag=test_dag
+)
+
+pytest.helpers.run_task(task=is_light, dag=test_dag)
+
+
+
+
